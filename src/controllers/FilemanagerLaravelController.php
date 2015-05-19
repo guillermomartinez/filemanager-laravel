a900ef36bd71dc670202ad1716a5e99896a3a0c2
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2015-05-19 11:29:32 -0500

    Use the correct Controller

--- src/controllers/FilemanagerLaravelController.php
+++ src/controllers/FilemanagerLaravelController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
+
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
 use Pqb\FilemanagerLaravel\FilemanagerLaravel;
